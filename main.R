241316383656edd31bdc7583ac647548d35b287c
Mael-GN <mael.grelneau@gmail.com>
2023-06-12 14:05:14 +0200

    Construire des séries chronologiques

--- main.R
+++ main.R
@@ -1,2 +1,9 @@
-c <- c(1:2)
-print(c)
+data = read.csv('stat_acc_V3.csv', sep=';')
+
+#Création d'une colonne mois dans le dataframe
+data$mois = month(data$date)
+
+#Création d'une week mois dans le dataframe contenant le numéro de la semaine
+data$week = week(data$date)
+
+plot(data$week)

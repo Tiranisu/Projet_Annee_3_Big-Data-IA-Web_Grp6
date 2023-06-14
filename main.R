dd98dbf3e830ebced268e7796cf4e08bbc7453f7
Mael-GN <mael.grelneau@gmail.com>
2023-06-15 00:41:35 +0200

    implémentation fonction prep + commentaire + clean code

--- main.R
+++ main.R
@@ -19,15 +19,28 @@ data <- read.csv('data/stat_acc_V3.csv', sep=';')
 villes <- read.csv('data/laposte_hexasmal.csv', sep=';')
 
 
+#Pour la partie preparation 2
+
+#CSV pour obtenir le nom de la région et du département à partir du code INSEE
+#https://static.data.gouv.fr/resources/communes-de-france-base-des-codes-postaux/20200309-131459/communes-departement-region.csv
+data_com_dep_reg <- read.csv('data/communes-departement-region.csv', sep=',')
+
+#CSV pour obtenir le nombre d'habitants par région
+#https://www.insee.fr/fr/statistiques/4265429?sommaire=4265511
+data_reg <- read.csv('data/regions.csv', sep=';')
+
+
 
 #------------------------------------------------------#
 #                                                      #
 #              Préparation des données                 #
 #                                                      #
 #------------------------------------------------------#
+source("Preparation_data.R")
+
 
 #Pour connaitres les catégorie d'une colonne
-unique(data$descr_grav)     
+unique(data$descr_grav)
 unique(data$descr_cat_veh)
 
 #On remplace les valeurs manquantes dans la colonne "place" par 0 (qu'on considère donc piéton)
@@ -47,35 +60,8 @@ villes$longitude <- sapply(strsplit(as.character(villes$coordonnees_geographique
 data$latitude <- villes$latitude[match(data$id_code_insee, villes$Code_commune_INSEE)]
 data$longitude <- villes$longitude[match(data$id_code_insee, villes$Code_commune_INSEE)]
 
-#Codage des description de la gravité, on passe d'une chaine de charactere à un chiffre : ACHTUNG! Aussi considéré comme char
-data$descr_grav <- fct_recode(data$descr_grav, "0" = "Indemne", "1" = "Blessé léger", "2" = "Blessé hospitalisé", "3" = "Tué")
+preparation_variables_multimodales(data)
 
-#Codage des descriptions des catégorie des véhicules, on passe d'une chaine de charactere à un chiffre :  ACHTUNG! Aussi considéré comme char
-data$descr_cat_veh <- fct_recode(data$descr_cat_veh, 
-  "0" = "PL seul > 7,5T",                                                                               
-  "1" = "VU seul 1,5T <= PTAC <= 3,5T avec ou sans remorque ",                                          
-  "2" = "VL seul",                                                                                      
-  "3" = "Autocar",                                                                                      
-  "4" = "PL > 3,5T + remorque",                                                                       
-  "5" = "Cyclomoteur <50cm3",                                                                          
-  "6" = "Motocyclette > 125 cm3",                                                                       
-  "7" = "Tracteur routier + semi-remorque",                                                             
-  "8" = "Tracteur agricole",                                                                            
-  "9" = "PL seul 3,5T <PTCA <= 7,5T",                                                                   
-  "10" = "Autobus",                                                                                      
-  "11" = "Scooter > 50 cm3 et <= 125 cm3",                                                               
-  "12" = "Train",                                                                                        
-  "13" = "Scooter > 125 cm3",                                                                            
-  "14" = "Scooter < 50 cm3",                                                                             
-  "15" = "Voiturette (Quadricycle à moteur carrossé) (anciennement \"voiturette ou tricycle à moteur\")",
-  "16" = "Autre véhicule",                                                                               
-  "17" = "Bicyclette",                                                                                   
-  "18" = "Motocyclette > 50 cm3 et <= 125 cm3",                                                          
-  "19" = "Engin spécial",                                                                                
-  "20" = "Quad lourd > 50 cm3 (Quadricycle à moteur non carrossé)",                                      
-  "21" = "Tramway",                                                                                      
-  "22" = "Tracteur routier seul",                                                                        
-  "23" = "Quad léger <= 50 cm3 (Quadricycle à moteur non carrossé)") 
 
 #On met les variables numeriques sous format numérique et date en date
 #ACHTUNG ! Peut etre fais buguer la suite du code si vous aviez considéré vos valeurs en string (elle ne le sont plus toute)
@@ -91,32 +77,36 @@ data$place <- as.numeric(data$place)
 data$descr_grav <- as.numeric(data$descr_grav)
 
 
-#Construction du jeu de données avec le nombre d'accidents selon la gravité p
-#Création d'une colonne mois dans la dataframe
-data$mois <- month(data$date)
-
-#Création d'une week mois dans le dataframe contenant le numéro de la semaine
-data$week <- week(data$date)
+#Construction des séries chronologiques sur l'évolution du nombre d'accidents par mois et par semaine en ajoutant 
+#une colonne "num_mois" et "num_semaine" contenant le numéro du mois et de la semaine de l'accident
+preparation_Ajout_Num_Mois_Semaines(data)
 
 
 #Construire un jeu de données avec le nombre d’accidents selon la gravité pour 100.000 habitants par région re d'accident par gravité
-data_com_dep_reg <- read.csv('data/communes-departement-region.csv', sep=',')
-#https://www.insee.fr/fr/statistiques/4265429?sommaire=4265511
-data_reg <- read.csv('data/regions.csv', sep=';')
 
+#On commmence par enlever tous les doublons de la base de données des communes, départements et régions pour éviter les doublons
 data_com_dep_reg <- data_com_dep_reg %>% group_by(code_commune_INSEE) %>% filter (! duplicated(code_commune_INSEE))
 
+#On enlève les colonnes inutiles
 data_com_dep_reg <- data_com_dep_reg[, c("code_commune_INSEE", "nom_departement", "nom_region")]
+#On enlève les collones inutiles
 data_reg <- data_reg[, c("REG", "PMUN")]
 
+#On merge les deux bases de données pour avoir le nom de la région et du département à partir du code INSEE
 data_total <- merge(data, data_com_dep_reg, by.x = "id_code_insee", by.y = "code_commune_INSEE")
 
+#On crée un jeu de données avec le nombre d'accident par gravité et par région
 ACP_data <- data_total %>% group_by(descr_grav, nom_region) %>% summarise(nb_acc = n()) %>% ungroup()
-accident_region <- data_total %>% group_by(nom_region) %>% count()
-accident_departement <- data_total %>% group_by(nom_departement) %>% count()
 
+#On merge les deux bases de données pour avoir le nombre d'habitants par région
 ACP_data <- merge(ACP_data, data_reg, by.x = "nom_region", by.y = "REG")
+#On crée une colonne avec le nombre d'accident pour 100.000 habitants grace à la colonne PMUN qui contient le nombre d'habitants par région
 ACP_data$nb_acc_pour_100000_hab <- (100000 * ACP_data$nb_acc) / ACP_data$PMUN
+
+#On crée un jeu de données avec le nombre d'accident par region, utilisé pour la carte
+accident_region <- data_total %>% group_by(nom_region) %>% count()
+##On crée un jeu de données avec le nombre d'accident par département, utilisé pour la carte
+accident_departement <- data_total %>% group_by(nom_departement) %>% count()
 
 
 #On remet les 9 premiers départements manquants dans la base d'accidents
@@ -139,8 +129,9 @@ accident_departement[nrow(accident_departement) + 1,] <- list("Ariège",NaN)
 #------------------------------------------------------#
 source("Visualisation_representation_graphique.R")
 source("Visualisation_histogrammes.R")
+
 #Pour preciser dans quel dossier on veut enrengistrer les graphs
-chemin = "C:/oui" 
+chemin <- "C:/oui"
 
 #Affichage des représentations graphiques
 visualisation_Nb_Acc_Athmo(chemin, data)
@@ -155,8 +146,8 @@ visualisation_Nb_Acc_Mois(chemin, data)
 
 #Affichage des cartes
 source("Visualisation_carte.R")
-visualisation_carte_region()
-visualisation_carte_departement()
+visualisation_carte_region(accident_region)
+visualisation_carte_departement(accident_departement)
 
 
 
